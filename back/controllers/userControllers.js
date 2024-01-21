6bc4dc59581b065d7f693b2b7f3da24fa73446b2
lytekm <kevin.morrison653@gmail.com>
2024-01-20 19:21:32 -0500

    set up front end

--- back/controllers/userControllers.js
+++ back/controllers/userControllers.js
@@ -1,5 +1,5 @@
-const User = require('../models/User');
-const bcrypt = require('bcryptjs');
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 const { JWTSECRET } = require("../config/env");
 
